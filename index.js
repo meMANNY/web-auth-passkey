a9c05c029108eaf7f4a5ffb8fd19507e09101e91
Aman Tripathi <amanttripathi02@gmail.com>
2024-05-18 18:12:28 +0530

    "created an inmemoruy database"

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ const port = 3000;
 
 const app = express();
 
+app.use(express.json());    // to support JSON-encoded bodies
+
+const userStore = {};
+
+app.post('/register',(req,res)=>{
+    const {username,password} = req.body; // destructuring  
+
+    const id = `${Date.now()}`;
+
+    const user = {
+        id,
+        username,
+        password
+    
+    }
+
+    userStore[id] = user;
+
+    console.log("registration successful",userStore[id]);
+
+    return res.json({id});
+})
 
 app.use(express.static('./public'));
 app.listen(port, () => {
