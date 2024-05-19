228fe9dc3aace7b8fd7c397ef19ac6503f83a16c
Aman Tripathi <amanttripathi02@gmail.com>
2024-05-19 14:30:54 +0530

    "debugged some error for generating passkey"

--- index.js
+++ index.js
@@ -15,45 +15,43 @@ app.use(express.json());    // to support JSON-encoded bodies
 app.use(express.static('./public'));
 const userStore = {};
 const challengeStore = {};
-app.post('/register',(req,res)=>{
-    const {username,password} = req.body; // destructuring  
-
-    const id = `user_${Date.now()}`;
+app.post('/register', (req, res) => {
+    const { username, password } = req.body
+    const id = `user_${Date.now()}`
 
     const user = {
         id,
         username,
         password
-    
     }
 
-    userStore[id] = user;
+    userStore[id] = user
 
-    console.log("registration successful",userStore[id]);
+    console.log(`Register successfull`, userStore[id])
 
-    return res.json({id});
+    return res.json({ id })
+
 })
 
-app.post("/register-challenge",async (req,res)=>{
+app.post('/register-challenge', async (req, res) => {
+    const { userId } = req.body
 
-    const {userId} = req.body;
+    if (!userStore[userId]) return res.status(404).json({ error: 'user not found!' })
 
-    if(!userStore[userId]){
-        return res.status(404).json({error:"User not found"});
-    }
+    const user = userStore[userId]
 
-    const user = userStore[userId];
-
-    //create the challenge
-    const challengePayload =  await generateRegistrationOptions({
+    const challengePayload = await generateRegistrationOptions({
         rpID: 'localhost',
-        rpName: 'My localhost machine',
-        username: user.username,
-    });
-    // store the challenge
+        rpName: 'My Localhost Machine',
+        
+        userName: user.username,
+    
+    })
+
     challengeStore[userId] = challengePayload.challenge
 
-    return res.json({options: challengePayload});
+    return res.json({ options: challengePayload })
+
 })
 
 app.post("/register-verify", async(req,res)=>{
